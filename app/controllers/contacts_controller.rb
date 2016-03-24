ea9a60322f813e9d880309a1fe1737578d58521c
Kovalev Evgeniy <e.evilgen@gmail.com>
2016-03-24 21:28:53 +0300

    Save contact on submit

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,6 +4,15 @@ class ContactsController < ApplicationController
   end
 
   def create
-    
+    @contact = Contact.new(contacts_params)
+    @contact.save 
   end
+
+  private
+
+#явно разрешаем использование параметров формы (сделано в целях защиты из вне)
+  def contacts_params
+    params.require(:contact).permit(:email, :message)
+  end
+
 end
