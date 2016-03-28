a3e059ab98e1de8a3ae695dc7e6985a3ec6d1314
Kovalev Evgeniy <e.evilgen@gmail.com>
2016-03-28 18:26:22 +0300

    Create pages controller,add terms & about view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'home/index'
-  get 'home/terms'
-  get 'home/about'
+
+  get 'terms' => 'pages#terms'
+  get 'about' => 'pages#about'
 
   resource  :contacts, only: [:new, :create], path_names: { :new => '' }
   resources :articles
